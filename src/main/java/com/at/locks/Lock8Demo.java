fec72c3aa046f68afbb53f95a9d2b8bfd9f69c20
zero <zero@zero.com>
2022-07-04 23:03:33 +0800

    sendEmail sendSMS 两个同步方法 sendEmail暂停3s -> 先打印邮件

--- src/main/java/com/at/locks/Lock8Demo.java
+++ src/main/java/com/at/locks/Lock8Demo.java
@@ -41,6 +41,7 @@ public class Lock8Demo {
 class Phone {
 
     public synchronized void sendEmail() {
+        try { TimeUnit.SECONDS.sleep(3); } catch (InterruptedException e) { e.printStackTrace(); }
         System.out.println("sendEmail ...");
     }
 
