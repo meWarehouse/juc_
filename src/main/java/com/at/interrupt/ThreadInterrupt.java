4b2fe1dcef54622c267b56a6f656511df77e14fe
zero <zero@zero.com>
2022-07-10 21:55:26 +0800

    线程中断 AtomicBoolean 原子引用

--- src/main/java/com/at/interrupt/ThreadInterrupt.java
+++ src/main/java/com/at/interrupt/ThreadInterrupt.java
@@ -2,6 +2,7 @@ package com.at.interrupt;
 
 
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * @create 2022-07-10
@@ -10,7 +11,40 @@ public class ThreadInterrupt {
 
     private static volatile boolean isStop = false;
 
+    private static AtomicBoolean atomicBoolean = new AtomicBoolean(false);
 
+
+    /**
+     * AtomicBoolean 原子引用
+     */
+    public static void m2(String[] args) {
+
+        new Thread(() -> {
+
+            while (true){
+                if(atomicBoolean.get()){
+                    System.out.println(String.format("... %s 线程被中断",Thread.currentThread().getName()));
+                    break;
+                }
+                System.out.println(Thread.currentThread().getName() + " 线程 running...");
+            }
+
+        },"A").start();
+
+        try { TimeUnit.SECONDS.sleep(1); } catch (InterruptedException e) { e.printStackTrace(); }
+
+        new Thread(() -> {
+            atomicBoolean.set(true);
+            System.out.println("线程 " + Thread.currentThread().getName() + " 将 线程 A 中断");
+        },"B").start();
+
+        System.out.println("main thread running.....");
+
+    }
+
+    /**
+     * volatile 可见性
+     */
     public static void m1(String[] args) {
 
         new Thread(() -> {
