38d4b0af29107e2d743ca5ca7a9e1d2328d1f9f1
zero <zero@zero.com>
2022-07-10 17:19:10 +0800

    synchronize 静态同步方法字节码

--- src/main/java/com/at/locks/SyncBytecode.java
+++ src/main/java/com/at/locks/SyncBytecode.java
@@ -34,10 +34,29 @@ public class SyncBytecode {
             0       9     0  this   Lcom/at/locks/SyncBytecode;
 
      */
-    public synchronized void syncMethod(){
-        System.out.println("... 同步方法 ...");
-    }
+//    public synchronized void syncMethod(){
+//        System.out.println("... 同步方法 ...");
+//    }
 
+
+    /*
+  public static synchronized void syncMethod();
+    descriptor: ()V
+    flags: ACC_PUBLIC, ACC_STATIC, ACC_SYNCHRONIZED
+    Code:
+      stack=2, locals=0, args_size=0
+         0: getstatic     #2                  // Field java/lang/System.out:Ljava/io/PrintStream;
+         3: ldc           #3                  // String ... 静态同步方法 ...
+         5: invokevirtual #4                  // Method java/io/PrintStream.println:(Ljava/lang/String;)V
+         8: return
+      LineNumberTable:
+        line 43: 0
+        line 44: 8
+
+     */
+    public static synchronized void syncMethod(){
+        System.out.println("... 静态同步方法 ...");
+    }
 
 
 
