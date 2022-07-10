48902ddff7ad6b361fcd0d6bf2d358dcdcd35949
zero <zero@zero.com>
2022-07-10 17:17:32 +0800

    synchronize 同步方法字节码

--- src/main/java/com/at/locks/SyncBytecode.java
+++ src/main/java/com/at/locks/SyncBytecode.java
@@ -5,6 +5,7 @@ package com.at.locks;
  */
 public class SyncBytecode {
 
+/*
     Object object = new Object();
 
     public void syncCodeBlock(){
@@ -13,6 +14,31 @@ public class SyncBytecode {
             throw new RuntimeException("...exception");
         }
     }
+*/
+
+    /*
+  public synchronized void syncMethod();
+    descriptor: ()V
+    flags: ACC_PUBLIC, ACC_SYNCHRONIZED
+    Code:
+      stack=2, locals=1, args_size=1
+         0: getstatic     #2                  // Field java/lang/System.out:Ljava/io/PrintStream;
+         3: ldc           #3                  // String ... 同步方法 ...
+         5: invokevirtual #4                  // Method java/io/PrintStream.println:(Ljava/lang/String;)V
+         8: return
+      LineNumberTable:
+        line 20: 0
+        line 21: 8
+      LocalVariableTable:
+        Start  Length  Slot  Name   Signature
+            0       9     0  this   Lcom/at/locks/SyncBytecode;
+
+     */
+    public synchronized void syncMethod(){
+        System.out.println("... 同步方法 ...");
+    }
+
+
 
 
 }
