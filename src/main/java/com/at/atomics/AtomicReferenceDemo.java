32a56f1edfb7c4e92376fee19f76b5ecfc32cbd9
zero <zero@zero.com>
2022-07-18 23:00:58 +0800

    引用类型原子类 AtomicReference

---
+++ src/main/java/com/at/atomics/AtomicReferenceDemo.java
@@ -0,0 +1,90 @@
+package com.at.atomics;
+
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicReference;
+
+/**
+ * @create 2022-07-18
+ */
+public class AtomicReferenceDemo {
+
+    /*
+
+        AtomicReference
+        AtomicStampedReference
+
+
+     */
+
+
+    public static void main(String[] args) {
+
+        User z3 = new User("z3", 10);
+        User l4 = new User("l4", 11);
+        User w5 = new User("w5", 12);
+
+        AtomicReference<User> atomicReference = new AtomicReference<>();
+
+        atomicReference.set(z3);
+
+
+        System.out.println(atomicReference.compareAndSet(z3,l4) + " -> " + atomicReference.get());
+        System.out.println(atomicReference.compareAndSet(z3,w5) + " -> " + atomicReference.get());
+
+
+
+    }
+
+}
+
+
+class User{
+
+    private String name;
+    private int age;
+
+    public User() {
+    }
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return age == user.age && Objects.equals(name, user.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
