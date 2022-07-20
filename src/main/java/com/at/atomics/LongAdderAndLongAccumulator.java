494ccf14616ad8a2f21bcb942fdb49ba46c0672e
zero <zero@zero.com>
2022-07-20 23:40:10 +0800

    原子操作增强类 DoubleAdder -> DoubleAccumulator，LongAdder -> LongAccumulator

--- src/main/java/com/at/atomics/LongAdderAndLongAccumulator.java
+++ src/main/java/com/at/atomics/LongAdderAndLongAccumulator.java
@@ -9,6 +9,27 @@ import java.util.function.LongBinaryOperator;
  */
 public class LongAdderAndLongAccumulator {
 
+    /*
+
+        DoubleAdder -> DoubleAccumulator
+        LongAdder -> LongAccumulator
+
+            void add(long x) 将当前的value加x
+            void increment() 将当前的value加1
+            void decrement() 将当前的value减1
+
+            long sum() 返回当前值，注意，在没有并发更新value的情况下，sum会返回i一个精确值，
+                                        存在并发的情况下，sum返回一个精确值
+
+
+
+            1    热点商品点赞计算器，点赞数加加统计，不要求实时精确
+            2    一个很大的List，里面都是int类型，如何实现加加，说说思路
+
+
+     */
+
+
     LongAdder longAdder = new LongAdder();
 
     public void add() {
