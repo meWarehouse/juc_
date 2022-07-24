5580ac11c2b09a0974cee7fb7b5fc6cfc1f0def5
zero <zero@zero.com>
2022-07-24 13:26:32 +0800

    偏向锁 mark work 演示

--- src/main/java/com/at/lockupgrade/LockUpgrade.java
+++ src/main/java/com/at/lockupgrade/LockUpgrade.java
@@ -7,7 +7,77 @@ import org.openjdk.jol.info.ClassLayout;
  */
 public class LockUpgrade {
 
-    public static void main(String[] args) {
+
+    public static void biasedLock(String[] args) {
+
+
+        /*
+
+            查看偏向锁相关的默认参数 java -XX:+PrintFlagsInitial |grep BiasedLock*
+
+            intx BiasedLockingBulkRebiasThreshold          = 20                                  {product}
+            intx BiasedLockingBulkRevokeThreshold          = 40                                  {product}
+            intx BiasedLockingDecayTime                    = 25000                               {product}
+            intx BiasedLockingStartupDelay                 = 4000                                {product}
+            bool TraceBiasedLocking                        = false                               {product}
+            bool UseBiasedLocking                          = true                                {product}
+
+
+            实际上偏向锁在JDK1.6之后是默认开启的，但是启动时间有延迟，所以需要添加参数-XX:BiasedLockingStartupDelay=0，让其在程序启动时立刻启动。
+
+            开启偏向锁：
+                -XX:+UseBiasedLocking -XX:BiasedLockingStartupDelay=0
+
+            关闭偏向锁：关闭之后程序默认会直接进入------------------------------------------>>>>>>>>   轻量级锁状态
+                -XX:-UseBiasedLocking
+
+         */
+
+        Object o = new Object();
+
+        // -XX:+UseBiasedLocking -XX:BiasedLockingStartupDelay=0
+        new Thread(() -> {
+            synchronized (o){
+                System.out.println(ClassLayout.parseInstance(o).toPrintable());
+            }
+        }).start();
+
+        /*
+java.lang.Object object internals:
+ OFFSET  SIZE   TYPE DESCRIPTION                               VALUE
+      0     4        (object header)                           e0 f4 5e 30 (11100000 11110100 01011110 00110000) (811529440)
+      4     4        (object header)                           00 00 00 00 (00000000 00000000 00000000 00000000) (0)
+      8     4        (object header)                           e5 01 00 f8 (11100101 00000001 00000000 11111000) (-134217243)
+     12     4        (loss due to the next object alignment)
+Instance size: 16 bytes
+Space losses: 0 bytes internal + 4 bytes external = 4 bytes total
+
+         */
+
+
+        /*
+
+-XX:+UseBiasedLocking -XX:BiasedLockingStartupDelay=0
+
+java.lang.Object object internals:
+ OFFSET  SIZE   TYPE DESCRIPTION                               VALUE
+      0     4        (object header)                           05 c8 62 2f (00000101 11001000 01100010 00101111) (795002885)
+      4     4        (object header)                           00 00 00 00 (00000000 00000000 00000000 00000000) (0)
+      8     4        (object header)                           e5 01 00 f8 (11100101 00000001 00000000 11111000) (-134217243)
+     12     4        (loss due to the next object alignment)
+Instance size: 16 bytes
+Space losses: 0 bytes internal + 4 bytes external = 4 bytes total
+
+         */
+
+
+    }
+
+
+
+
+
+    public static void noLock(String[] args) {
 
 
         Object o = new Object();
